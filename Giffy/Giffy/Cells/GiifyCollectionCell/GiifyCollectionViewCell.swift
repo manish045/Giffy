8fe8a133ef9c53f036026ea8c3bf2e734ba3f60c
manish045 <manish@canbrand.in>
2022-06-15 15:05:50 +0530

    set up loader and gif collection cell setup

---
+++ Giffy/Giffy/Cells/GiifyCollectionCell/GiifyCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  GiifyCollectionViewCell.swift
+//  Giffy
+//
+//  Created by Manish Tamta on 15/06/2022.
+//
+
+import UIKit
+
+class GiifyCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var gifImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Giffy/Giffy/Controller/View/GiffyListViewController.swift
+++ Giffy/Giffy/Controller/View/GiffyListViewController.swift
@@ -8,11 +8,58 @@
 import UIKit
 
 class GiffyListViewController: UIViewController {
+    
+    enum Section: Int, CaseIterable {
+        case gif
+        case loader
+    }
  
     var viewModel: DefaultGiffyListViewModel!
- 
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
-        
+        super.viewDidLoad()
+        configureCollecitonView()
+    }
+    
+    private func configureCollecitonView() {
+        self.collectionView.registerNibCell(ofType: LoadingCollectionCell.self)
+        self.collectionView.registerNibCell(ofType: GiifyCollectionViewCell.self)
+        self.collectionView.dataSource = self
     }
 }
+
+extension GiffyListViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return Section.allCases.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch Section(rawValue: section) {
+        case .gif:
+            return 5
+            
+        case .loader:
+            return 1
+            
+        default:
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        switch Section(rawValue: indexPath.section) {
+        case .gif:
+            let loadingCell = collectionView.dequeueCell(GiifyCollectionViewCell.self, indexPath: indexPath)
+            return loadingCell
+
+        default:
+            print("default cell")
+            let loadingCell = collectionView.dequeueCell(LoadingCollectionCell.self, indexPath: indexPath)
+            return loadingCell
+        }
+    }
+}
+
