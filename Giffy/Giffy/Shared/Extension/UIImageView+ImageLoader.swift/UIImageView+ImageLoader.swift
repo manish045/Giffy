72750858772fb77b0c3cd0fd9c5b08b35736fbdb
manish045 <manish@canbrand.in>
2022-06-16 13:19:14 +0530

    load more data and remove unwanted library

--- Giffy/Giffy/Shared/Extension/UIImageView+ImageLoader.swift/UIImageView+ImageLoader.swift
+++ Giffy/Giffy/Shared/Extension/UIImageView+ImageLoader.swift/UIImageView+ImageLoader.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
-import SwiftyGif
+import SDWebImage
+import FLAnimatedImage
 
-extension UIImageView {
+extension FLAnimatedImageView {
     
     func downloadImage(from url: String?,
                        placeholderImage: UIImage?) {
@@ -20,7 +21,25 @@ extension UIImageView {
         }
 
         if let transformedUrl = URL(string: originalUrlString) {
-            self.setGifFromURL(transformedUrl)
+            
+            sd_internalSetImage(with: transformedUrl, placeholderImage: placeholderImage, options: SDWebImageOptions(rawValue: 0), context: nil, setImageBlock: { [weak self] (image, imageData, _, _)  in
+                guard let strongSelf = self else { return }
+                
+                let imageFormat = NSData.sd_imageFormat(forImageData: imageData)
+                if imageFormat == .GIF {
+                    DispatchQueue.global(qos: .userInteractive).async { [weak self] in
+                        let animatedImage = FLAnimatedImage(animatedGIFData: imageData)
+                        DispatchQueue.main.async { [weak self] in
+                            guard let strongSelf = self else { return }
+                            strongSelf.animatedImage = animatedImage
+                            strongSelf.image = nil
+                        }
+                    }
+                } else {
+                    strongSelf.image = image
+                    strongSelf.animatedImage = nil
+                }
+                }, progress: nil, completed: nil)
         }
     }
 }
